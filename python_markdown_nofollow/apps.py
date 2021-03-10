2e8583847fb1b01fe2cb7ab9321738104ad47816
stdworkflow <79711562+stdworkflow@users.noreply.github.com>
2021-03-11 05:55:12 +0800

    Add files via upload

---
+++ python_markdown_nofollow/apps.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# _*_ coding:utf-8 _*_
+# __author__ = '__lyp__'
+
+from django.apps import AppConfig
+
+__title__ = 'python_markdown_nofollow.apps'
+__author__ = 'lyp <gsyipingliu@gmail.com>'
+__copyright__ = '2021Artur lyp'
+__license__ = 'MIT'
+__all__ = ('Config',)
+
+
+class Config(AppConfig):
+    """Config."""
+
+    name = 'python_markdown_nofollow'
+    label = 'python_markdown_nofollow'
